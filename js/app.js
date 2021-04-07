94494da8d08bcf2955529221e5337a478ff8fe74
ssungs <asdf4651@naver.com>
2021-04-07 17:13:28 +0900

    feat: Update Code
    Update code app.js
    
    Add function toggleProperty

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
+// Main page
+
 $selectLists = document.querySelector('.select-list');
 
+const toggleProperty = e => {
+  if (e.target.style.cssText === 'z-index: 0;') {  
+    e.target.previousElementSibling.style.setProperty('z-index', 1)
+  } else if (e.target.style.cssText === 'z-index: 1;') {  
+    e.target.style.setProperty('z-index', 0)
+  }
+}
 
 $selectLists.onclick = e => {
   if (e.target === $selectLists) return;
-  [...$selectLists.children].forEach($selectList => {
-    $selectList.classList.toggle('hide', $selectList === e.target);
-  });
+  toggleProperty(e);
 }
