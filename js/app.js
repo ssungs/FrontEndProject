9b5e3cf7aeb4ef9cc71e9f53aaa6d2af816cff8c
ebchoi <eunbichoi@outlook.com>
2021-04-08 15:34:03 +0900

    CSS separated for each page

--- js/app.js
+++ js/app.js
@@ -7,59 +7,50 @@ $selectLists.onclick = e => {
   });
 }
 
-
-const carousel = ($container, images) => {
-    let currentSlide = 0;
-    let isMoving = false;
-    const DURATION = 500; 
-
-    const timerId = null;
-    let $carouselSlides = null;
-
-    const move = (currentSlide, duration = 0) => {
-        if (duration) isMoving = true;
-        $carouselSlides.style.setProperty('--duration', duration);
-        $carouselSlides.style.setProperty('--currentSlide', currentSlide);
+const carousel = ($carouselContainer, images) => {
+  let currentSlide = 0;
+  let isMoving = false;
+  const DURATION = 500;
+  const move = (currentSlide, duration = 0) => {
+    if (duration) isMoving = true;
+    $carouselSlides.style.setProperty('--duration', duration);
+    $carouselSlides.style.setProperty('--currentSlide', currentSlide);
+  };
+  document.addEventListener('DOMContentLoaded', () => {
+    $carouselContainer.innerHTML = `
+    <div class="carousel">
+      <div class="carousel-slides">
+          ${[images[images.length - 1], ...images, images[0]].map(url => `<img src="${url}" />`).join('')}
+      </div>
+      </div>
+      <button class="carousel-control prev">&laquo; previous</button>
+      <button class="carousel-control next">next &raquo;</button>
+      `;
+    $carouselSlides = document.querySelector('.carousel-slides');
+  });
+  window.onload = () => {
+    const { offsetWidth } = $carouselSlides.querySelector('img');
+    $container.style.width = `${offsetWidth}px`;
+    move(++currentSlide);
+    $container.style.opacity = 1;
+  };
+  $carouselContainer.onclick = ({ target }) => {
+    if (!target.classList.contains('carousel-control') || isMoving) return;
+    const differentiator = target.classList.contains('prev') ? -1 : 1;
+    currentSlide += 1 * differentiator;
+    move(currentSlide, DURATION);
+  };
+  $carouselContainer.ontransitionend = () => {
+    isMoving = false;
+    const differentiator = currentSlide === 0 ? 1 : currentSlide === images.length + 1 ? -1 : 0;
+    if (!differentiator) return;
+    currentSlide += images.length * differentiator;
+    move(currentSlide);
     };
-
-    document.addEventListener('DOMContentLoaded', () => {
-        $container.innerHTML = `
-        <div class="carousel-slides">
-            ${[images[images.length - 1], ...images, images[0]].map(url => `<img src="${url}" />`).join('')}
-        </div>
-        `;
-
-        $carouselSlides = document.querySelector('.carousel-slides');
-    });
-
-    window.onload = () => {
-      const { offsetWidth } = $carouselSlides.querySelector('img');
-      $container.style.width = `${offsetWidth}px`;
-
-        move(++currentSlide);
-        $container.style.opacity = 1;
-    };
-
-    $container.onclick = ({ target }) => {
-        if (!target.classList.contains('carousel-control') || isMoving) return;
-        const differentiator = target.classList.contains('prev') ? -1 : 1;
-        currentSlide += 1 * differentiator;
-        move(currentSlide, DURATION);
-    };
-
-    $container.ontransitionend = () => {
-        isMoving = false;
-        const differentiator = currentSlide === 0 ? 1 : currentSlide === images.length + 1 ? -1 : 0;
-        if (!differentiator) return;
-
-        currentSlide += images.length * differentiator;
-        move(currentSlide);
-    };
-    };
-
-    carousel(document.querySelector('.carousel'), [
-    '/images/banner_css.png',
-    '/images/banner_html.jpeg',
-    '/images/banner_js.png',
-    '/images/banner_python.png'
-    ]);
+};
+carousel(document.querySelector('.carousel-container'), [
+  '/images/banner_css.png',
+  '/images/banner_html.jpeg',
+  '/images/banner_js.png',
+  '/images/banner_python.png'
+]);
