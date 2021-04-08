066b67be1449f93309b4ab76af05475dfd8620b3
downside154 <blue_tree154@hotmail.com>
2021-04-08 15:05:21 +0900

    feature: carousel
    Finalized carousel on html to updated commentary
    Changed carousel styling on css.
    changed carousel on app.js to move the scope of the carousel-control
    buttons.

--- js/app.js
+++ js/app.js
@@ -1,15 +1,14 @@
 // Main page
-
 $selectLists = document.querySelector('.language-lists');
+let $carousel = document.querySelector('.carousel')
+let $carouselSlides = document.querySelector('.carousel-slides')
 
-
-const carousel = ($container, images) => {
+const carousel = ($carouselContainer, images) => {
   let currentSlide = 0;
   let isMoving = false;
   const DURATION = 500;
 
-  const timerId = null;
-  let $carouselSlides = null;
+
 
   const move = (currentSlide, duration = 0) => {
     if (duration) isMoving = true;
@@ -18,45 +17,44 @@ const carousel = ($container, images) => {
   };
 
   document.addEventListener('DOMContentLoaded', () => {
-    $container.innerHTML = `
+    $carouselContainer.innerHTML = `
+    <div class="carousel">
       <div class="carousel-slides">
           ${[images[images.length - 1], ...images, images[0]].map(url => `<img src="${url}" />`).join('')}
       </div>
+      </div>
+      <button class="carousel-control prev">&laquo; previous</button>
+      <button class="carousel-control next">next &raquo;</button>
       `;
-
     $carouselSlides = document.querySelector('.carousel-slides');
   });
 
   window.onload = () => {
-    const {
-      offsetWidth
-    } = $carouselSlides.querySelector('img');
+    const { offsetWidth } = $carouselSlides.querySelector('img');
     $container.style.width = `${offsetWidth}px`;
 
     move(++currentSlide);
     $container.style.opacity = 1;
   };
 
-  $container.onclick = ({
-    target
-  }) => {
+  $carouselContainer.onclick = ({ target }) => {
     if (!target.classList.contains('carousel-control') || isMoving) return;
     const differentiator = target.classList.contains('prev') ? -1 : 1;
     currentSlide += 1 * differentiator;
     move(currentSlide, DURATION);
   };
-
-  $container.ontransitionend = () => {
+  
+  $carouselContainer.ontransitionend = () => {
     isMoving = false;
     const differentiator = currentSlide === 0 ? 1 : currentSlide === images.length + 1 ? -1 : 0;
     if (!differentiator) return;
-
+  
     currentSlide += images.length * differentiator;
     move(currentSlide);
-  };
+    };
 };
 
-carousel(document.querySelector('.carousel'), [
+carousel(document.querySelector('.carousel-container'), [
   '/images/banner_css.png',
   '/images/banner_html.jpeg',
   '/images/banner_js.png',
@@ -76,3 +74,21 @@ $selectLists.onclick = e => {
   toggleProperty(e);
 }
 
+
+
+
+// const polaroids = document.querySelector('.polaroids');
+// const polaroid = document.querySelector('.polaroids .polaroid');
+// const leftButton = document.querySelector('.carousel-left');
+// const rightButton = document.querySelector('.carousel-right');
+// let currentPolaroid = 0;
+// const movePolaroid = num => {
+//     polaroids.style.left = -num * 380 + 'px';
+//     currentPolaroid = num;
+// }
+// rightButton.onclick = () => {
+//     currentPolaroid < 3 ? movePolaroid(currentPolaroid + 1) : movePolaroid(0);
+// }
+// leftButton.onclick = () => {
+//     currentPolaroid > 0 ? movePolaroid(currentPolaroid - 1) : movePolaroid(3);
+// }
